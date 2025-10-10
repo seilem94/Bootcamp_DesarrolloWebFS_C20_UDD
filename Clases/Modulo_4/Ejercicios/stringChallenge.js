548fa2c71e4e38d6072a5ae42c9aae01bbc9e093
ESS-Salem <ESS@DESKTOP-QVAQUM3>
2025-10-10 16:57:19 -0300

    Refactor stringChallenge function for consistency and improve validation logic

--- Clases/Modulo_4/Ejercicios/stringChallenge.js
+++ Clases/Modulo_4/Ejercicios/stringChallenge.js
@@ -1,4 +1,4 @@
-const stringChallenge = (str) => {
+const StringChallenge = (str) => {
 
     //Hay que ingresar un texto y comprobar el formato XXX.XXX.XXX
     //const stringEntrada = prompt("Ingrese la cadena a evaluar:");
@@ -6,19 +6,13 @@ const stringChallenge = (str) => {
     const grupos = str_nospaces.split("."); //Separa la cadena en 3 partes.
     const numeros = grupos.join(""); // Une las partes en una sola cadena sin puntos.
 
-    //Validaciones; se comprobara el largo del texto, que las posiciones [3] y [7] sean puntos y que el resto sean numeros.
+    //Validacion de formato; se comprobara el largo del texto, que las posiciones [3] y [7] sean puntos y que el resto sean numeros.    
     for (let i = 0; i < numeros.length; i++) {
         if (isNaN(numeros[i]) && numeros[i]!== "0") {
             //console.log(`El formato de la cadena debe ser XXX.XXX.XXX \n Donde cada 'X' es un dígito del 1 al 9 (sin ceros).`);
             console.log("false");
             return false;
         }
-    }
-    
-    if (str_nospaces.length !== 11) {
-        //console.log("Largo incorrecto. El formato de la cadena debe ser XXX.XXX.XXX");
-        console.log("false");
-        return false;
     }
 
     if (str_nospaces[3] !== "." || str_nospaces[7] !== ".") {
@@ -27,12 +21,27 @@ const stringChallenge = (str) => {
         return false;
     }
 
+    //Verificación Condicion 1
+    if (str_nospaces.length !== 11) {
+        //console.log("Largo incorrecto. El formato de la cadena debe ser XXX.XXX.XXX");
+        console.log("false");
+        return false;
+    }
     //Si la cadena es correcta, se procede a comprobar las condiciones para retornar true o false.
     //Utilizamos el arreglo 'grupos' para validar cada grupo de números.
     const grupos_parsed = grupos.map(grupo => parseInt(grupo, 10));
-    
-};
+    grupo_1 = grupos_parsed[0];
+    grupo_2 = grupos_parsed[1];
+    grupo_3 = grupos_parsed[2];
 
+    const ultimoDigitoMayor = (grupo) => {
+        (grupo[2] > grupo[1] && grupo[2] > grupo[0]) ? true : false;
+    } 
+    if (ultimoDigitoMayor(grupo_1) && ultimoDigitoMayor(grupo_2) && ultimoDigitoMayor(grupo_3)) { //Verificacion Condicion 4
+
+
+    };
+}
 // 🔹 Captura argumento desde consola
-const entrada = process.argv[2];
-stringChallenge(entrada);
+//const entrada = process.argv[2];
+//stringChallenge(entrada);
