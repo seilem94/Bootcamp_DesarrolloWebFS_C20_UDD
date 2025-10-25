8850bc02958136cf704717750da29a05ec0cfc9c
Seilem94 <salemhidd1994@gmail.com>
2025-10-25 11:10:53 -0300

    Add nodemon as a dev dependency and configure environment variables for server port

--- Clases/Modulo_4/clase_16/expressUserUdd/src/main.js
+++ Clases/Modulo_4/clase_16/expressUserUdd/src/main.js
@@ -1,9 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import {env} from './config/env.config.js'
 
 //Objetivo abrir el puerto
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`Servidor escuchando en el puerto ${3000}`);
+const {port} = env;
+//const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
+
+//Se instala nodemon para que el servidor se reinicie automaticamente
+//npm install nodemon --save-dev (o -D)
+//--save-dev se usa solo para usarla como dependencia para desarrollo (y no produccion)
+
